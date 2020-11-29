c0659be22b816b133060903f18c4617d9760450d
Anamil Mehta <anamil@seas.upenn.edu>
2020-11-28 20:15:39 -0800

    Made changes in the T for end effecotr - now IK works and robot successfully grabs and picks up the objec

--- Main/final.m
+++ Main/final.m
@@ -1,20 +1,22 @@
 function final(color)
-%     color = 'blue';
+     color = 'blue';
     global lynx % necessary to use ArmController inside a function
     lynx = ArmController(color);
 
     pause(1)
 
     % interact with simulator, such as...
-
+     q1=[-1, 0, 0, 0, 0, 0];
+    fprintf('\n here \n\n\n');
+    lynx.set_pos(q1);
+    disp(' there')
     % get state of your robot
     [q,qd]  = lynx.get_state()
-
     % get state of scoreable objects
    [name,pose,twist] = lynx.get_object_state();
 
-%    q=[0.9, pi/4, -0.3, -0.3, -pi/2, 0];
-    lynx.set_pos(q); % used to set position to q
+   
+    % used to set position to q
     
 % % %   get state of your opponent's robot
   %  [q,qd]  = lynx.get_opponent_state()
