fab53d54c01e2c82af585244d8134fecdaeee942
unknown <shaunclfedrick@gmail.com>
2020-11-30 19:02:15 -0500

    added function to detect boxes in your face

--- Rotation/Strike/striketester.m
+++ Rotation/Strike/striketester.m
@@ -5,5 +5,8 @@ pause(1)
 tic
 %cube static 2
 %cube static 6
-[WithInFace,BoxesInUrFace] = InUrFace(lynx,color);
+Range=100;
+Direction=0.7;
+Axis=3;
+[WithInFace,BoxesInUrFace] = InUrFace(lynx,color,Range,Direction,Axis);
 toc
