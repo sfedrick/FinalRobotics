d4cd874e831a3a4207585027690e638170d320bc
Anamil Mehta <anamil@seas.upenn.edu>
2020-11-30 22:58:50 -0800

    adjusted pauses/timing for the latest gazebo update + made two goal positions for 2 stacks of 2 blocks each + added intermediate safe stroke to qEnd after each iteration; TESTED in auto mode = good

--- Statics/static.m
+++ Statics/static.m
@@ -3,7 +3,9 @@ function [] = static(color)
     global lynx
     lynx = ArmController(color);
     % get state of your robot
+    
     pause(2)
+    
     [q,qd]  = lynx.get_state()
 
     % get state of scoreable objects
@@ -13,12 +15,12 @@ function [] = static(color)
    if strcmp(color, 'blue')
        Trg = [-1 0 0 200; 0 -1 0 200; 0 0 1 0; 0 0 0 1];  % transf matrix from ground to robot base.
        goal = [ 30; 470; 40];
-       goal_trans = [0, -1, 0, 110; -1, 0, 0, -285; 0, 0, -1, 100; 0, 0, 0, 1];
+       goal_trans = [0, -1, 0, 110; -1, 0, 0, -285; 0, 0, -1, 60; 0, 0, 0, 1];
        
    elseif strcmp(color, 'red')
        Trg = [1 0 0 200; 0 1 0 200; 0 0 1 0; 0 0 0 1];
        goal = [ -30; -470; 40];  
-       goal_trans = [0, -1, 0, 110; -1, 0, 0, -285; 0, 0, -1, 100; 0, 0, 0, 1];
+       goal_trans = [0, -1, 0, 110; -1, 0, 0, -285; 0, 0, -1, 60; 0, 0, 0, 1];
        
    else
        error('Sorry, wrong color name!')
@@ -65,66 +67,68 @@ function [] = static(color)
         static.pose{i,1} = pose{priority(i,:)};
     end
     %picking static block
-     celldisp(static.name)
-    celldisp(static.pose)
+%      celldisp(static.name)
+%     celldisp(static.pose)
     
 
 %     Tinput = [0, 1, 0, ax; 0, -1, 0, ay; -1, 0, 0, az; 0, 0, 0, 1];
-
+ for i=1:4
     %get the coordinate of the robot base in ground
     base = inv(Trg) * [0;0;0;1] 
     base = base(1:3);
-    T_pick_g = PickedPose(static.pose{1}, base, 50); %desired picked pose in ground frame
+    T_pick_g = PickedPose(static.pose{i}, base, 50); %desired picked pose in ground frame
     T_pick_r = Trg * T_pick_g  ;           %desired picked pose in robot frame                
     q1 = calculateIK(T_pick_r);
-%     if isempty(q1)
-%         Tinput = [0, 1, 0, ax; 0, -1, 0, ay; -1, 0, 0, az; 0, 0, 0, 1];
     q1 = [q1, 20]
     
-    lynx.set_pos(q1);
-    pause(3)
+    lynx.command(q1);
+    pause(1)
     [q ~] = lynx.get_state();
-    pause(2)
+    
     reach = 0;
     % reach 50 mm above the object
     while (reach == 0)
         [q, ~] = lynx.get_state()
-        pause(0.2)
-        lynx.set_pos(q1)
+        pause(0.1)
+        lynx.command(q1)
         disp("reaching")
         reachNorm = norm(q-q1)
-        if norm(q - q1) < 0.5
+        if reachNorm < 0.1
             reach = 1;
+        else
+            lynx.command(q1)
         end
     end
     
     disp("reach complete");
-    pause(5)
+    pause(0.5)
     
-    T_down_g = T_pick_g - [zeros(3), [0;0;40];0 0 0 0];
+    T_down_g = T_pick_g - [zeros(3), [0;0;35];0 0 0 0];
     T_down_r = Trg * T_down_g  ;           %desired picked pose in robot frame                
     qdown = calculateIK(T_down_r);
     qdown = [qdown 20];
-    lynx.set_pos(qdown);
+    lynx.command(qdown);
+    pause(1)
     dive = 0;
         
     while (dive == 0)
         [q, ~] = lynx.get_state()
          diveNorm = norm(q - qdown)
         disp("diving")
-        if norm(q - qdown) < 0.5
+        if norm(q - qdown) < 0.2
             dive = 1;
-            
+        else
+            lynx.command(qdown);
         end
     end
     disp("Dive complete");
-    
+    pause(1)
     %grab the object
     qGrab = [qdown(1:5), -15];
-    pause(5)
-    lynx.set_pos(qGrab);
+    
+    lynx.command(qGrab);
     grab = 0;
-    pause(5)
+    pause(1)
     
 
     
@@ -132,62 +136,72 @@ function [] = static(color)
     [qpick, ~] = calculateIK(Tpick);
     qPick = [qpick, -15]
     %qPick
-    lynx.set_pos(qPick);
+    lynx.command(qPick);
+    pause(0.5)
     pick = 0;
-    i=0;
     while (pick == 0)
-        i=i+1;
-        if i==50
-            break
-        end
         [q, ~] = lynx.get_state()
         disp("picking")
         qPick
         pickNorm = norm(q(1:5)-qPick(1:5))
         if pickNorm < 0.5
             pick = 1;
+        else
+        lynx.command(qPick);
         end
-        lynx.set_pos(qPick);
     end
     disp("Pick complete");
     
+    %stacks of more than 2 are risky as any slight movement by robot
+    %will make them fall. Hence, made 2 different positions for
+    %two stacks of 2 static blocks
     Move  = 0; goal_trans
-    Tplace = goal_trans;
+    if i<3
+        Tplace = goal_trans + [ zeros(1,3), -40; zeros(1,4); 0, 0, 0, (i * 20); zeros(1,4)];
+    else
+        Tplace = goal_trans + [ zeros(1,3), +10; zeros(1,4); 0, 0, 0, ((i-2) * 20); zeros(1,4)];
+    end
     [qPlace, ~] = calculateIK(Tplace);
     qPlace = [qPlace, -15];
-    lynx.set_pos(qPlace)
-    i=0;
+    lynx.command(qPlace)
+    pause(2)
     while (Move == 0)
         [q, ~] = lynx.get_state();
-        lynx.set_pos(qPlace);
         qPlace
-        i=i+1;
-        if i>50
-            break
-        end
         moveNorm = norm (q(1:5) - qPlace(1:5))
-        if moveNorm < 0.5
+        if moveNorm < 0.6
             Move = 1;
+%         else
+%             lynx.command(qPlace);
         end
     end
     
     disp("Move complete");
     TDrop = goal_trans;
     [qDrop, ~] = calculateIK(TDrop);
-    qDrop = [qDrop, 30];
-    lynx.set_pos(qDrop)
+    qDrop = [qPlace(1:5), 30];
+    pause(1)
+    lynx.command(qDrop)
+    pause(1)
     drop = 0;
     
     while (drop == 0)
         [q, ~] = lynx.get_state();
-        if norm(q - qDrop) < 0.5
+        normDrop = norm(q - qDrop)
+        if normDrop < 0.5
             drop = 1;
+        else
+            lynx.command(qDrop);
         end
     end
     
     disp("Object Dropped");
-    %move back up
-    lynx.set_pos(qPlace);
+    %move to qEnd. qEnd acts as an intermediate position so that
+    %the robot doesnot hit a block while moving to another block
+    %this is done by retracting the robot upwards towards qEnd
+    qEnd = [0, 0,qPlace(3:5), 30];
+    lynx.command(qEnd);
+    pause(1)
     
     disp(" Placed static object ");
     % Tranformation matrix at goal
@@ -195,7 +209,7 @@ function [] = static(color)
     
     sDone =1;
    % lynx.set_pos(q);
-    
+ end   
 %%% manual testss end
  % %   get state of your opponent's robot
  %   [q,qd]  = lynx.get_opponent_state()
