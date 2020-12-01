5079cd42a928988bd8a58fed33a95f2179aba8d6
anamilmehta <74869758+anamilmehta@users.noreply.github.com>
2020-11-30 16:15:18 -0800

    Revert "Statics"

--- Main/final.m
+++ Main/final.m
@@ -1,19 +1,16 @@
 function final(color)
+    % color = 'blue';
     global lynx % necessary to use ArmController inside a function
     lynx = ArmController(color);
 
     pause(1)
 
     % interact with simulator, such as...
-     q1=[0 0 0 0 0 0];
-     fprintf('\n here \n\n\n');
-     lynx.set_pos(q1);
-    disp(' there')
-    pause(2)
+
     % get state of your robot
     [q,qd]  = lynx.get_state()
-    % get state of scoreable objects
 
+    % get state of scoreable objects
    [name,pose,twist] = lynx.get_object_state();
 
   %q=[0.9, pi/4, -0.3, -0.3, -pi/2, 0];
@@ -29,7 +26,6 @@ lynx.set_pos(q); % used to set position to q
 [q,qd]  = lynx.get_state()
 % % %   get state of your opponent's robot 
 %[q,qd]  = lynx.get_opponent_state()
-
 
 
 end
